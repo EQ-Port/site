da3792ce723ca60f11ee45981c87502d28fce5cf
ogursan <ogursan@yandex.ru>
2014-10-31 00:24:30 +0400

    assets removed

--- assets/compiled/footer-58e91210ef301d1a911a281c9d16ec22.js
+++
@@ -1,4 +0,0 @@
-// Built 2014-10-31 00:20:43
-// sample.js
-someAction();
-
--- assets/compiled/header-82718aa214168240aed26e0258a8067b.js
+++
@@ -1,6 +0,0 @@
-// Built 2014-10-31 00:20:43
-// lib/some-function.js
-function someAction() {
-    console.log('Some Action!');
-}
-
