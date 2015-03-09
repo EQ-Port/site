2637c65a2f24a0cb1192636a2b76271584a2128f
Леонид Некин <kmzslroot@gmail.com>
2015-03-10 01:06:23 +0300

    Модель для мероприятий

---
+++ protected/models/Events.php
@@ -0,0 +1,16 @@
+<?
+
+class Events extends CActiveRecord
+{
+        public static function model($className = __CLASS__)
+        {
+                return parent::model($className);
+        }
+
+        public function tableName()
+        {
+                return 'events';
+        }
+}
+
+?>
